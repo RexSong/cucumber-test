2a554437044a5a33203fdff0419156e2a27ef749
Rex.Song <westerncampus@gmail.com>
2020-08-07 19:29:28 +0800

    add structure for cucumber java

---
+++ src/test/java/com/rex/cucumber/sample/cucumbertest/steps/TestStep.java
@@ -0,0 +1,24 @@
+package com.rex.cucumber.sample.cucumbertest.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TestStep {
+
+    public TestStep() {
+
+    }
+
+    @Given("^User login to website$")
+    public void userLoginToWebsite() {
+    }
+
+    @When("^user type xx$")
+    public void userTypeXx() {
+    }
+
+    @Then("^Display abc in page$")
+    public void displayAbcInPage() {
+    }
+}
---
+++ src/test/resources/features/Test.feature
@@ -0,0 +1,5 @@
+Feature: Test Feature
+  Scenario: Test Scenario
+    Given User login to website
+    When user type xx
+    Then Display abc in page
